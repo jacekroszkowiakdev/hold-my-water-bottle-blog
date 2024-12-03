140eef53df727b5ae9f7fdce380e257a9b744900
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-12-03 22:53:42 +0100

    fix(terraform): correct region variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,7 +4,11 @@ variable "vpc_id" {
   default = "aws_vpc.capstone_vpc.id"
 }
 
-variable "region" { description = "eu-central-1" }
+variable "region" {
+    description = "The region to use for the project"
+    type        = string
+    default = "eu-central-1"
+ }
 
 variable "az1" {
   description = "Primary availability zone"
