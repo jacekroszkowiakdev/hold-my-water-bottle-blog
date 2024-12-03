0f5fa112ccc5170ed8822f6c4c86f5ec4a757d4c
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-12-03 22:57:24 +0100

    fix(terraform): correct vpc id variable name in subnets

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "capstone_vpc" {
 
 # Subnet in availability zone 1
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = var.vpc.id
+  vpc_id                  = var.vpc_id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.az1
@@ -19,7 +19,7 @@ resource "aws_subnet" "public_subnet_1" {
 }
 
 resource "aws_subnet" "private_subnet_1" {
-  vpc_id            = var.vpc.id
+  vpc_id            = var.vpc_id
   cidr_block        = "10.0.2.0/24"
   availability_zone = var.az1
 
@@ -30,7 +30,7 @@ resource "aws_subnet" "private_subnet_1" {
 
 # Subnet in availability zone 2
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = var.vpc.id
+  vpc_id                  = var.vpc_id
   cidr_block              = "10.0.3.0/24"
   map_public_ip_on_launch = true
   availability_zone       = var.az2
@@ -41,7 +41,7 @@ resource "aws_subnet" "public_subnet_2" {
 }
 
 resource "aws_subnet" "private_subnet_2" {
-  vpc_id            = var.vpc.id
+  vpc_id            = var.vpc_id
   cidr_block        = "10.0.4.0/24"
   availability_zone = var.az2
 
