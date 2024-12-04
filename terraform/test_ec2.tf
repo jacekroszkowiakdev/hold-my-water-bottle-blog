af6fa13487b0d08376a66da8f05c8d37ebfbe8c9
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-12-04 16:25:31 +0100

    test(terraform): remove duplicate ami declaration

--- terraform/test_ec2.tf
+++ terraform/test_ec2.tf
@@ -1,12 +1,12 @@
-data "aws_ami" "amazon_linux2" {
-  most_recent = true
-  owners      = ["amazon"]
+# data "aws_ami" "amazon_linux2" {
+#   most_recent = true
+#   owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+#   }
+# }
 
 resource "aws_instance" "test_instance" {
   ami                    = data.aws_ami.amazon_linux2.id
