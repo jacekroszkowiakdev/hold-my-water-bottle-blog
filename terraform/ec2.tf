f56cf2f491902a6ae7c536baa6149f8da23dcae0
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-12-04 01:56:28 +0100

    fix(terraform): correct public key value

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@
 resource "aws_instance" "wordpress_instance" {
   ami                    = "ami-00d72ec36cdfc8a0a"
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.vockey.key_name
+  key_name               = "vockey"
   subnet_id             = aws_subnet.public_subnet_1.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.capstone_blog_sg.id]
