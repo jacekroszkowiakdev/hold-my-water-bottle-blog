d19fa9e4eb5c6906950ad9313eb20484befbe2bf
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2024-12-04 15:31:41 +0100

    test(terraform): change values and naming for test instance

--- terraform/test_ec2.tf
+++ terraform/test_ec2.tf
@@ -8,16 +8,16 @@ data "aws_ami" "amazon_linux2" {
   }
 }
 
-resource "aws_instance" "wordpress_instance" {
+resource "aws_instance" "test_instance" {
   ami                    = data.aws_ami.amazon_linux2.id
   instance_type          = "t2.micro"
   key_name               = "vockey"
-  subnet_id             = aws_subnet.public_subnet_1.id
+  subnet_id             = aws_subnet.public_subnet_2.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.capstone_blog_sg.id]
 
   tags = {
-    Name = "WordPress Blog Instance"
+    Name = "TEST Instance"
   }
 
   # Read the userData.sh file
